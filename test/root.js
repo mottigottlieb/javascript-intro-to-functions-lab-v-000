a4083dd3bc4fd784f1ca962073ce915a71a73be6
Motti <=>
2017-05-24 13:19:10 -0400

    Done.

--- test/root.js
+++ test/root.js
@@ -3,7 +3,7 @@ global.expect = require('expect');
 const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
-
+ 
 before(function(done) {
   const babelResult = babel.transformFileSync(
     path.resolve(__dirname, '..', 'index.js'), {
